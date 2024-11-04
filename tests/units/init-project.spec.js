685c8eff532567025deec8737a6d2f6b8e97b3be
khanh2906 <khanhphungdai@gmail.com>
2024-11-04 16:14:58 +0700

    update version 0.1.-beta

--- tests/units/init-project.spec.js
+++ tests/units/init-project.spec.js
@@ -10,19 +10,16 @@ describe('createDirectoryStructure', () => {
 	const packageJsonPath = path.join(projectPath, 'package.json');
 
 	beforeAll(async () => {
-		// Ensure the templatePath exists for testing
 		if (!await fs.pathExists(templatePath)) {
 			throw new Error(`Template path ${templatePath} does not exist`);
 		}
 	});
 
 	beforeEach(async () => {
-		// Clean up any previous test runs
 		await fs.remove(projectPath);
 	});
 
 	afterAll(async () => {
-		// Clean up after all tests
 		await fs.remove(projectPath);
 	});
 
@@ -38,16 +35,29 @@ describe('createDirectoryStructure', () => {
 		expect(packageJson.version).toBe(options.version);
 	});
 
-	it('should handle errors and throw an exception', async () => {
-		// Simulate an error by passing an invalid template path
-		const invalidTemplatePath = path.join(__dirname, 'invalid-template');
-		const createDirectoryStructureWithInvalidPath = async () => {
-			const originalTemplatePath = templatePath;
-			templatePath = invalidTemplatePath;
-			await createDirectoryStructure(projectPath, options);
-			templatePath = originalTemplatePath;
-		};
+	it('should work without a description and version', async () => {
+		const optionsWithoutDescriptionAndVersion = { projectName };
+		await createDirectoryStructure(projectPath, optionsWithoutDescriptionAndVersion);
 
-		await expect(createDirectoryStructureWithInvalidPath).rejects.toThrow();
+		const packageJson = await fs.readJson(packageJsonPath);
+		expect(packageJson.name).toBe(projectName);
+	});
+
+	it('should remove Dockerfile if not selected', async () => {
+		const optionsWithoutDocker = { projectName, description: 'Test project', version: '1.0.0', docker: false };
+		await createDirectoryStructure(projectPath, optionsWithoutDocker);
+
+		const dockerFilePath = path.join(projectPath, 'Dockerfile');
+		const dockerExists = await fs.pathExists(dockerFilePath);
+		expect(dockerExists).toBe(false);
+	});
+
+	it('should remove ESLint config if not selected', async () => {
+		const optionsWithoutEslint = { projectName, description: 'Test project', version: '1.0.0', eslint: false };
+		await createDirectoryStructure(projectPath, optionsWithoutEslint);
+
+		const eslintFilePath = path.join(projectPath, '.eslintrc.json');
+		const eslintExists = await fs.pathExists(eslintFilePath);
+		expect(eslintExists).toBe(false);
 	});
 });
